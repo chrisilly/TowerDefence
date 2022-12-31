28989046b248db1abf87c2416ca50fe7343fd95b
Blissling <unconfigured@null.spigotmc.org>
2022-12-31 14:53:59 +0100

    Debug tools

--- TowerDefence/Actor.cs
+++ TowerDefence/Actor.cs
@@ -11,13 +11,24 @@ namespace TowerDefence
     public class Actor
     {
         protected Texture2D texture;
+        protected Vector2 velocity;
+
         protected Vector2 position;
         public Vector2 Position { get { return position; } }
-        protected Vector2 velocity;
+
+        protected Rectangle hitbox;
+        public Rectangle Hitbox { get { return hitbox; } }
+        public Vector2 HitboxPosition { get { return new Vector2(hitbox.X, hitbox.Y); } }
 
         public virtual void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(texture, position, Color.White);
+            DrawHitbox(spriteBatch);
+        }
+
+        public void DrawHitbox(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(TextureManager.hitboxTexture, hitbox, Color.Green * 0.5f);
         }
 
         public virtual void Update(GameTime gameTime)
