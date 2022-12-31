ccf34106d5e4f612be784b8df57785101e8c96eb
Blissling <unconfigured@null.spigotmc.org>
2022-12-31 13:18:25 +0100

    AutoFire

--- TowerDefence/Tower.cs
+++ TowerDefence/Tower.cs
@@ -10,12 +10,64 @@ namespace TowerDefence
 {
     internal class Tower : Actor
     {
+        public static List<Tower> towers = new();
+
+        float bulletSpeed;
         float fireRate;
+        double fireCooldownTimer;
         bool autoFire;
 
-        public Tower(Texture2D texture) : base(texture)
+        public Tower()
         {
+            this.texture = TextureManager.towerTexture;
+            this.bulletSpeed = 200f;
+            this.fireRate = 3f;
+            this.fireCooldownTimer = fireRate;
+        }
 
+        public override void Update(GameTime gameTime)
+        {
+            AutoFire(gameTime);
+        }
+
+        public void Shoot()
+        {
+            Vector2 direction = GetShortestTrajectory();
+            Bullet bullet = new Bullet(position, bulletSpeed, direction);
+            Bullet.bullets.Add(bullet);
+        }
+
+        // Aim for the nearest enemy
+        public Vector2 GetShortestTrajectory()
+        {
+            Vector2 shortestPath = Vector2.Zero;
+
+            foreach (Enemy enemy in Enemy.enemies)
+            {
+                Vector2 pathToEnemy = enemy.Position - this.position;
+                if(shortestPath == Vector2.Zero)
+                {
+                    shortestPath = pathToEnemy;
+                }
+                else if(pathToEnemy.Length() < shortestPath.Length())
+                {
+                    shortestPath = pathToEnemy;
+                }
+            }
+
+            return Vector2.Normalize(shortestPath);
+        }
+
+        // Fire a bullet every couple of seconds
+        public void AutoFire(GameTime gameTime)
+        {
+            fireCooldownTimer += gameTime.ElapsedGameTime.TotalSeconds;
+
+            if(fireCooldownTimer >= fireRate )
+            {
+                fireCooldownTimer = 0;
+                Shoot();
+            }
         }
     }
 }
