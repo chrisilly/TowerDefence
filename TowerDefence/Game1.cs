a372955c80cc01bb8b8ba9f50010f26daa746fa9
Blissling <unconfigured@null.spigotmc.org>
2023-01-01 13:12:44 +0100

    Tower creation compatability fix

--- TowerDefence/Game1.cs
+++ TowerDefence/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -42,7 +43,7 @@ namespace TowerDefence
             foreach (Enemy enemy in Enemy.enemies.ToList())
                 enemy.Update(gameTime);
 
-            foreach (Tower tower in Tower.towers)
+            foreach (Tower tower in Tower.towers.ToList())
                 tower.Update(gameTime);
 
             foreach (Bullet bullet in Bullet.bullets)
